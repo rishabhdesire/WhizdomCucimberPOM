ebdace02674906e8cdcfe7fe74c9a7ab85a4f920
rishabhdesire <stolenheartbot@gmail.com>
2022-06-10 14:32:56 +0530

    Updated Car feature

--- CucumberPOM/src/test/java/steps/BuyingCar.java
+++ CucumberPOM/src/test/java/steps/BuyingCar.java
@@ -1,11 +1,18 @@
 package steps;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 
 public class BuyingCar {
 	
 	@Given ("I go to buy a car")
 	public void buy()
+	{
+		
+	}
+	
+	@And ("car must be of ford motors")
+	public void buyand()
 	{
 		
 	}
